440bf1216d7b2c6c39c3c88d7b7e8e5f18d583db
corpp112 <116607079+corpp112@users.noreply.github.com>
2023-02-13 22:09:51 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+(async() => {
+  await import('./index.mjs');
+})();
+//Hello! If you are reading this, that means that you forked my Replit and now have a working proxy. Please let me know if you liked the proxy by leaving a review in the comments. Also make sure to follow me and check out my other proxies. Thank you, and have a wonderful day!
